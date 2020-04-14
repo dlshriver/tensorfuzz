3866662e6f7d0aa23270e23e390215809926d127
David Shriver <dls2fc@virginia.edu>
2020-04-14 09:47:09 -0400

    remove extraneous echo

--- examples/localrobustness/tensorfuzz.sh
+++ examples/localrobustness/tensorfuzz.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo $(pwd)
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
